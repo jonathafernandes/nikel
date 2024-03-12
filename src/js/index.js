e24629038c79bb4616a6a13e7514129685c3bd80
jonathafernandes <jonathafernandes@live.com>
2024-03-12 09:22:05 -0300

    chore: changes folder structure

--- src/js/index.js
+++ src/js/index.js
@@ -27,7 +27,7 @@ document.getElementById("login-form").addEventListener("submit", function (e) {
 
         saveSession(email, checkSession);
 
-        window.location.href = "home.html";
+        window.location.href = "./src/pages/home.html";
     }
 
 });
